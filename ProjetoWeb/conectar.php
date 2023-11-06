2a7ab20df9729fac25e683d3ebede594419a9733
jtancon <101944685+jtancon@users.noreply.github.com>
2023-11-06 14:47:26 -0300

    Add files via upload

---
+++ ProjetoWeb/conectar.php
@@ -0,0 +1,12 @@
+<?php
+//variavel global para utilizar nas outras abas
+global $servername;
+global $username;
+global $password;
+global $database;
+// Dados do banco de dados 
+$servername = "localhost:3306";
+$username = "root";
+$password = "";
+$database = "biblioteca";
+?>
